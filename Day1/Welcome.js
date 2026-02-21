aafe9c1bfe309c5fedcf2c2396d1778e686f571f
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-21 19:12:15 +0300

    first commit

---
+++ Day1/Welcome.js
@@ -0,0 +1,12 @@
+const age=12;
+const AgaCal=()=>{
+    if(age<12){
+        console.log("you are teenager")
+    }
+    else if(age<=18){
+        console.log("you are youn ")
+    }
+    else{
+        console.log("you are old")
+    }
+    AgaCal()

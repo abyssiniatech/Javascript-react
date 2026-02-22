289dfa615d39110d13a03d3d9bd9409038e92e2b
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-22 15:11:13 +0300

    add excution context

--- Day2/Day2.js
+++ Day2/Day2.js
@@ -1,4 +1,5 @@
-let x=100;
+console.log(x) //these is undefind because before declaration no excution found these is excutoion context
+var x=100;
 let y=12;
 const addTwoNum=(num1,num2)=>{
     const sum = num1+num2

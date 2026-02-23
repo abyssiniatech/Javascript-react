ba30a342e6a834398f92a13201c286ab80a71e92
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-23 19:59:53 +0300

    add async/await with js

--- Day2/Promise3.js
+++ Day2/Promise3.js
@@ -16,3 +16,10 @@ promis1.then(message=>{
 promis1.catch(error=>{
     console.log(`somthing happend ${error}`)
 })
+
+const p=document.createElement("p");
+p.style.color="white";
+p.textContent="The first paragraph"
+const newDiv=document.createElement("div");
+newDiv.append(document.body)
+p.append(newDiv)
